22bf7a26819e8cde1522e80634c8d5e7971da7e7
Yanyu-G <gengyuki08042@gmail.com>
2026-09-12 22:02:22 -0400

    2.20

--- Week2/sketch1.js
+++ Week2/sketch1.js
@@ -1,10 +1,20 @@
-let y = 0;
+let x = 0;
 function setup(){
-    createCanvas(400,400)
+    createCanvas(500,500);
+    background(color('#f6f7d7'));
+    frameRate(2);
 }
 function draw(){
-    background(0);
-    rect(width/2,y,50,50);
-    y++;
-    print(y)
+    if (x >= width) {
+    x = 0;
+    background('#f6f7d7');
+}
+
+let buildheight = random(100, 300);
+let r = random(190, 230);
+let g = random(170, 210);
+let b = random(110, 150);
+fill(r,g,b);
+rect(x, height - buildheight, 100, buildheight);
+x = x + 100
 }
