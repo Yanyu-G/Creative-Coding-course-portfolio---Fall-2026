20774dd6aa935a9030ba6ac24f05b00dacdcf6ed
Yanyu-G <gengyuki08042@gmail.com>
2026-09-12 22:27:59 -0400

    2.24

--- Week2/sketch2.js
+++ Week2/sketch2.js
@@ -1,10 +1,39 @@
-let y = 0;
+let x = 0;
 function setup(){
-    createCanvas(windowWidth,windowHeight)
+    createCanvas(windowWidth, 500);
+    background(color('#2d4059'));
+    frameRate(10);
 }
 function draw(){
-    background(200);
-    rect(0,y,50,50);
-    y++;
-    print(y)
+background(45, 64, 89, 80);
+let lightSize = random(5, 30);
+   fill(random([
+    '#ffaaa5',
+    '#ffd3b6',
+    '#dcedc1',
+    '#a8e6cf',
+    '#cca8e9',
+    '#c3bef0',
+    '#cadefc',
+    '#defcf9'
+]));
+    noStroke();
+    rect(mouseX,mouseY,
+        lightSize,
+        lightSize
+    );
+
+let shapeType = floor(random(3));
+ if (shapeType == 0) {
+        rect(mouseX, mouseY, lightSize, lightSize);
+    } else if (shapeType == 1) {
+        circle(mouseX, mouseY, lightSize);
+    } else {
+        triangle(
+            mouseX, mouseY - lightSize,
+            mouseX - lightSize, mouseY + lightSize,
+            mouseX + lightSize, mouseY + lightSize
+        );
+
+    }
 }
