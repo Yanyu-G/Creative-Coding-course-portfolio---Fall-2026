ced72c17e3a3e64bb57b729a4e306074f6554fda
Yanyu-G <gengyuki08042@gmail.com>
2026-09-12 23:08:24 -0400

    2.30

--- Week2/sketch4.js
+++ Week2/sketch4.js
@@ -5,15 +5,19 @@ function setup(){
     frameRate(10);
 }
 function draw() {
+background('#111827');
 for (let x = 0; x < width; x += 60) 
 {let buildingHeight =220 + sin(frameCount * 0.05 + x * 0.02) * 100;
 fill('#243447');
+ if (mouseX > x && mouseX < x + 50) {fill('#3b5068');} 
+ else {fill('#243447');}
 noStroke();
 rect(x,height - buildingHeight,50,buildingHeight);
-for (let y = height - buildingHeight + 20; y < height - 20; y += 30) {
+if (mouseX > x && mouseX < x + 50) {
   fill('#ffd166');
-  noStroke()
+  for (let y = height - buildingHeight + 20; y < height - 20; y += 30) {
   rect(x + 10, y, 8, 12);
-  rect(x + 30, y, 8, 12);}
+}
+}
 }
 }
