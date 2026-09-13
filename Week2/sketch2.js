baab9f61a12d58a00bd49d116ef61d662583a4c7
Yanyu-G <gengyuki08042@gmail.com>
2026-09-12 22:40:21 -0400

    2.27

--- Week2/sketch2.js
+++ Week2/sketch2.js
@@ -15,25 +15,12 @@ let lightSize = random(5, 30);
     '#cca8e9',
     '#c3bef0',
     '#cadefc',
-    '#defcf9'
-]));
+    '#defcf9']));
     noStroke();
-    rect(mouseX,mouseY,
-        lightSize,
-        lightSize
-    );
+    rect(mouseX,mouseY,lightSize,lightSize);
 
 let shapeType = floor(random(3));
- if (shapeType == 0) {
-        rect(mouseX, mouseY, lightSize, lightSize);
-    } else if (shapeType == 1) {
-        circle(mouseX, mouseY, lightSize);
-    } else {
-        triangle(
-            mouseX, mouseY - lightSize,
-            mouseX - lightSize, mouseY + lightSize,
-            mouseX + lightSize, mouseY + lightSize
-        );
-
-    }
+ if (shapeType == 0) {rect(mouseX, mouseY, lightSize, lightSize);
+    } else if (shapeType == 1) {circle(mouseX, mouseY, lightSize);
+    } else {triangle(mouseX, mouseY - lightSize,mouseX - lightSize, mouseY + lightSize,mouseX + lightSize, mouseY + lightSize);}
 }
--- Week2/sketch3.js
+++ Week2/sketch3.js
@@ -1,10 +1,20 @@
-let y = 0;
+let x = 0;
 function setup(){
-    createCanvas(windowWidth,windowHeight)
+    createCanvas(windowWidth, 500);
+    background(color('#f6f7d7'));
+    frameRate(5);
 }
 function draw(){
-    background(200);
-    rect(y,40,50,50);
-    y++;
-    print(y)
+let buildheight = random(100, 450);
+
+rect(mouseX,height - buildheight,50,buildheight);
+   let buildingColor = random([
+    '#e8c07d',
+    '#d9a273',
+    '#c9b98b',
+    '#b7c4a0',
+    '#d6c6a5']);
+fill(buildingColor);
+stroke('#9e927d');
+strokeWeight(1);
 }
